b6c926eac0cfebb4fb3c55ecf899baaae942d164
shanmukha <shanuthanudevkam@gmail.com>
2026-02-06 16:15:28 +0530

    Created function to generate and return token

--- src/main/java/com/example/login/service/StudentService.java
+++ src/main/java/com/example/login/service/StudentService.java
@@ -1,5 +1,6 @@
 package com.example.login.service;
 
+import com.example.login.component.TokenStore;
 import com.example.login.dto.LoginRequestDto;
 import com.example.login.dto.StudentRequestDto;
 import com.example.login.dto.StudentResponseDto;
@@ -8,19 +9,23 @@ import com.example.login.repository.StudentRepository;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.UUID;
 
 @Service
 public class StudentService {
 
     private final StudentRepository repo;
     private final PasswordEncoder encoder;
+    private final TokenStore tokenStore;
 
-    public StudentService(StudentRepository repo, PasswordEncoder encoder){
+    public StudentService(StudentRepository repo, PasswordEncoder encoder, TokenStore tokenStore){
         this.repo = repo;
         this.encoder = encoder;
+        this.tokenStore = tokenStore;
     }
 
     public void addStudent(@RequestBody StudentRequestDto dto){
@@ -33,7 +38,6 @@ public class StudentService {
     }
 
     public List<StudentResponseDto> getAllStudent(){
-
         List<StudentResponseDto> studentResponseList;
 
         studentResponseList = repo.findAll().stream()
@@ -57,5 +61,23 @@ public class StudentService {
         Student std = optionalstd.get();
 
         return encoder.matches(dto.getPassword(), std.getPassword());
+    }
+
+    public String loginToken(LoginRequestDto dto){
+
+        Optional<Student> optionalstd = repo.findByUsn(dto.getUsn());
+
+        if(optionalstd.isEmpty())
+            return "User not found";
+
+        Student std = optionalstd.get();
+
+        if(!encoder.matches(dto.getPassword(),std.getPassword()))
+            return "Invalid password";
+
+        String token = UUID.randomUUID().toString();
+        tokenStore.store(token,std.getName());
+
+        return token;
     }
 }
