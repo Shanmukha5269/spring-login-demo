78aabfa3f8de19ee22fe1a589bb3eb2df3b00b59
shanmukha <shanuthanudevkam@gmail.com>
2026-02-06 16:17:13 +0530

    Authorized the request mapping

--- src/main/java/com/example/login/config/SecurityConfig.java
+++ src/main/java/com/example/login/config/SecurityConfig.java
@@ -14,6 +14,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/login").permitAll()
                         .requestMatchers("/students").permitAll()
+                        .requestMatchers("/get-token").permitAll()
                         .anyRequest().authenticated()
                 );
         return http.build();
